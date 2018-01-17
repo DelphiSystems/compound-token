88a606b8a4e686becb910c0dd072e0f09c3dcead
Delphi Systems <contact@delphi.systems>
2018-01-17 01:27:32 +0100

    Add pragma version to SignalToken and MinimalToken

--- contracts/MinimalToken.sol
+++ contracts/MinimalToken.sol
@@ -1,3 +1,5 @@
+pragma solidity ^0.4.18;
+
 contract Token {
     uint256 public totalSupply;
 
--- contracts/SignalToken.sol
+++ contracts/SignalToken.sol
@@ -1,3 +1,5 @@
+pragma solidity ^0.4.18;
+
 /*
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
